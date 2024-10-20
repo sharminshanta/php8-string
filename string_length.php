96dcfa0d831fe5983b2e92a766a2c45d88dd40dd
Sharmin Shanta <mimetyshanta@gmail.com>
2024-10-20 13:30:20 +0600

    #4: String Concatenation.

---
+++ string_length.php
@@ -0,0 +1,12 @@
+<?php
+
+# Issue Reference: https://github.com/sharminshanta/php8-string/issues/4
+
+/**
+ * String Length: strlen() or without built-in function
+ * The strlen() function returns the length(number of characters) of a string.
+ */
+$str = "Hello, Programming!";
+$length = strlen($str);
+
+echo $length;  // Output: 19
