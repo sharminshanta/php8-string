e96b2e007ce22d5f962c9a0597ac59e68354b428
Sharmin Shanta <mimetyshanta@gmail.com>
2024-10-23 14:40:19 +0600

    #18: String html_special_chars.

--- html_special_chars.php
+++ html_special_chars.php
@@ -5,4 +5,9 @@
  * HTML Entities: htmlspecialchars()
  * The htmlspecialchars() function is used to convert special characters (such as <, >, and &)
  * into HTML entities, preventing cross-site scripting (XSS) attacks.
+ *
+ * This is particularly useful when displaying user input in a web page.
  */
+$str = "<h1>Hello, programmers!</h1>";
+echo htmlspecialchars($str);
+// Output: &lt;h1&gt;Hello, programmers!&lt;/h1&gt;
