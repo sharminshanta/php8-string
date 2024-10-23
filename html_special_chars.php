286f76b9b66b2f3ba7c6db19ff1c792f16f6acea
Sharmin Shanta <mimetyshanta@gmail.com>
2024-10-23 14:38:38 +0600

    #18: String html_special_chars.

---
+++ html_special_chars.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Issue : https://github.com/sharminshanta/php8-string/issues/18
+ * HTML Entities: htmlspecialchars()
+ * The htmlspecialchars() function is used to convert special characters (such as <, >, and &)
+ * into HTML entities, preventing cross-site scripting (XSS) attacks.
+ */
