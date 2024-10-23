197c077dfe58691298c17a6e2331b8c81c223815
Sharmin Shanta <mimetyshanta@gmail.com>
2024-10-23 14:36:46 +0600

    #17: String formatting.

--- string_formatting.php
+++ string_formatting.php
@@ -15,4 +15,9 @@ $name = "Devid";
 $age = 30;
 
 $formatted = sprintf("His name is %s and he is %d years old.", $name, $age);
-echo $formatted;  // Output: His name is Devid and he is 30 years old.
+// echo $formatted;  // Output: His name is Devid and he is 30 years old.
+
+// We can also control the number of decimal places in floats using %.2f.
+$price = 99.99;
+echo sprintf("The price is %.2f", $price);  // Output: The price is 99.99
+
